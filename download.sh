996c1b560163d2f753c7fa04e215122f6fbde005
jhkim105 <just090323!>
2020-12-30 09:29:36 +0900

    download.sh

--- download.sh
+++ download.sh
@@ -11,9 +11,8 @@ ARTIFACT=$1
 OUTPUT_DIRECTORY=$2
 
 function download_file() {
-  DEST=$HOME_DIR/$SERVICE_NAME.$PACKAGE
   echo "download file...ARTIFACT:$ARTIFACT OUTPUT_DIRECTORY:$OUTPUT_DIRECTORY"
-  mvn org.apache.maven.plugins:maven-dependency-plugin:3.0.0:copy -Dartifact=$ARTIFACT -DoutputDirectory=$OUTPUT_DIRECTORY -Dmdep.useBaseVersion=true
+  mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.2:copy -Dartifact=$ARTIFACT -DoutputDirectory=$OUTPUT_DIRECTORY -Dmdep.useBaseVersion=true
   echo "download complete..."
 }
 
